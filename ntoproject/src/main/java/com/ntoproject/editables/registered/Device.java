bea4ebbc90aad85a9189adc81b0c6e6a7ff9f9e4
icedragon2008 <ice_dragon_black@mail.ru>
2026-03-05 21:45:52 +0300

    Part of S-1

--- ntoproject/src/main/java/com/ntoproject/editables/registered/Device.java
+++ ntoproject/src/main/java/com/ntoproject/editables/registered/Device.java
@@ -3,12 +3,17 @@ package com.ntoproject.editables.registered;
 import com.bpa4j.core.Data.Editable;
 import com.bpa4j.editor.EditorEntry;
 import com.bpa4j.editor.Input;
-@Input(nameProvider = )
+@Input(nameProvider = Device.NameProvider.class)
 public class Device extends Editable{
 	public Device(){
 		super("Нов Оборудование");
 	}
-	
+	public static class NameProvider implements com.bpa4j.editor.NameProvider{
+		public String provideName(Editable editable){
+			Device e=(Device)editable;
+			return e.model + "-" +  e.num;
+		}
+	}
 	@EditorEntry(translation="Модель")
 	public String model;
 	@EditorEntry(translation="Гаражный номер")
--- ntoproject/src/main/java/com/ntoproject/editables/registered/WorkNorm.java
+++ ntoproject/src/main/java/com/ntoproject/editables/registered/WorkNorm.java
@@ -1,5 +1,9 @@
 package com.ntoproject.editables.registered;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.function.Function;
+
 import com.bpa4j.core.Data.Editable;
 import com.bpa4j.editor.EditorEntry;
 
@@ -7,4 +11,16 @@ public class WorkNorm extends Editable{
 	public WorkNorm(){
 		super("Нов Установка норм горных работ");
 	}
+	@EditorEntry(translation="Дата регистрации норм горных работ")
+	public LocalDate date;
+	@EditorEntry(translation="Вид работы")
+	public WorkType workType;
+	@EditorEntry(translation="Обородувоние")
+	public static Function<WorkNorm,ArrayList<Device>> device=e->e.workType.device;
+	@EditorEntry(translation="Продукция")
+	public static Function<WorkNorm,Nomencl>product=e->e.workType.product;
+	@EditorEntry(translation="Норма за смену")
+	public int norm;
+	@EditorEntry(translation="Единицы измерения")
+	public static Function<WorkNorm,Function<WorkType,Unit>>unit=e->e.workType.units;
 }
